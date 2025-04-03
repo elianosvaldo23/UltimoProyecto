e3c81e7e4637249c0a9d70bec994dcb0de179533
elianosvaldo23 <elianverdeciamesa@gmail.com>
2025-04-03 11:49:43 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuración de la base de datos MySQL
+DB_CONFIG = {
+    'host': 'postgresql://mysql_ultimoproyecto_user:SDjsCKP0OrItfULvHXQl5GarqbCgDGso@dpg-cvnainadbo4c73bh6rlg-a/mysql_ultimoproyecto',  # El host que te proporciona Render
+    'user': 'mysql_ultimoproyecto_user',             # Usuario de la base de datos
+    'password': 'SDjsCKP0OrItfULvHXQl5GarqbCgDGso',      # Contraseña de la base de datos
+    'db': 'mysql_ultimoproyecto',         # Nombre de la base de datos
+    'port': 5432                      # Puerto (generalmente 3306 para MySQL)
+}
